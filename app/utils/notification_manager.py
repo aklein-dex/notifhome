b6e8cf55bf1f1aad251ce5c1bcfd231185d630d8
Alex <alexandre.klein@tanukisoftware.com>
2017-08-22 16:25:17 +0900

    add detection of platform

--- app/utils/notification_manager.py
+++ app/utils/notification_manager.py
@@ -1,4 +1,5 @@
 import sys
+import platform
 from datetime import datetime
 from app.utils.file_manager import create_file
 from app.models.user import User
@@ -6,22 +7,31 @@ from app.models.notification import Notification
 
 
 def process_notification(user, message, light= 1, sound = 1):
+    success = False
     notification = Notification(user, message, datetime.now(), light, sound)
     
     if not notification.is_valid():
         return False
     
-    # create a file
+    # create all type of notifications
     if create_file(notification):
-        return True
+        
+        if is_omega2():
+            # write on OLED screen
     
-    # do a check if we are on the Omega2 or on a "real" computer
-    # write on OLED screen
+            # make sound
+        
+            # turn on light
+            success = True
+        else:
+            success = True
     
-    # make sound
-    
-    # turn on light
-    
-    return False
+    return success
 
     
+def is_omega2():
+    """ Check if the current machine is an Omega2.
+        I couldn't find any other way that is 100% accurate.
+    """
+    return platform.uname()[4] == 'Mips'
+        
